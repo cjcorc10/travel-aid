55b0eb603e57dd835405a4c099fcfc8709af19f1
Kyle C <xkcorc@gmail.com>
2025-04-01 00:27:00 -0400

    adding prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ */
+module.exports = {
+  // use single quotes instead of double quotes
+  singleQuote: true,
+  // omit semicolons at the end of statements
+  semi: false,
+  // allow trailing commas for multi-line objects and arrays (not function parameters)
+  trailingComma: 'es5',
+}
