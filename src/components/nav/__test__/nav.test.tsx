492f62956815eb8d5542371b83659b1d4f5d7337
Kyle C <xkcorc@gmail.com>
2025-04-02 22:59:23 -0400

    add nav test

---
+++ src/components/nav/__test__/nav.test.tsx
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest'
+
+describe('Nav', () =>
+  it('should render', () => {
+    // redner the component
+    // find the elment use testing playground ext
+    // assert the element logic
+  }))
