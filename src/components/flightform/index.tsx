c7469d1ad101c5f08f0230f92ad5a0167821d8fb
reddish <cjcorc10@gmail.com>
2025-04-05 14:48:19 -0500

    added form

---
+++ src/components/flightform/index.tsx
@@ -0,0 +1,68 @@
+import { useForm, SubmitHandler } from "react-hook-form"
+import { useState } from "react"
+
+interface Inputs {
+    departing: string
+    destination: string
+    from: Date
+    to?: Date
+    travelers: number
+}
+
+type TripType = "round-trip" | "one-way"
+
+
+const FlightForm = () => {
+    const { register, handleSubmit} = useForm<Inputs>({defaultValues: {travelers: 1}})
+    //const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+    const [roundTrip, setRoundTrip] = useState<TripType>("round-trip");
+  return (
+    <form 
+        className="bg-white flex flex-col border border-emerald-500 rounded-lg shadow-lg"
+        onSubmit={handleSubmit((data) => {
+            console.log(data);
+        })}>
+        <div className="flex flex-col p-8">
+            <select
+                className="border-3 border-emerald-500 rounded-lg px-2 p-1 mb-7"
+                value={roundTrip}
+                onChange={(e) => setRoundTrip(e.target.value as TripType)}>
+                <option value="round-trip">Round-trip</option>
+                <option value="one-way">One-way</option>
+            </select>
+            <label
+                className="text-gray-600"
+                >From</label>
+            <input {...register("departing")}
+                className="border border-gray-300 rounded-lg mb-4 p-1 px-2"/>
+            <label
+                className="text-gray-600"
+                >To</label>
+            <input {...register("destination")}
+                className="border border-gray-300 rounded-lg mb-4 p-1 px-2"/>
+            <label
+                className="text-gray-600">Travelers</label>
+                <input {...register("travelers")}
+                    className="border border-gray-300 p-1 px-2 rounded-lg w-10 mb-4"/>
+            <label
+                className="text-gray-600">Date from</label>
+                <input type="date" {...register("from")}
+                    className="border border-gray-300 rounded-lg mb-4 p-1 px-2"/>
+            {roundTrip == "round-trip" && 
+                <>
+                    <label
+                        className="text-gray-600">Date to</label>
+                    <input type="date" {...register("to")}
+                        className="border border-gray-300 rounded-lg mb-4 p-1 px-2"/>
+                </>
+            }
+        </div>
+        <input 
+            className="bg-emerald-600 overflow-clip rounded-b-lg p-2 text-green-50 font-bold hover:bg-emerald-700"
+            type="submit" />
+    </form>
+  )
+}
+
+export default FlightForm
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { MockResponse } from '../../mocks/handlers'
+import FlightForm from '../../components/flightform'
 import Hero from '../../components/Hero'
 
 const Home = () => {
@@ -23,19 +24,20 @@ const Home = () => {
   return (
     <div className="bg-gray-50 font-ubuntu flex justify-center p-4">
       <div className="flex flex-col md:flex-row mx-8 w-full max-w-7xl">
-      <Hero />
-      <div className="flex-1 flex flex-col items-center justify-center gap-4">
-        <h1 className="text-3xl font-bold underline text-emerald-700">App</h1>
-        <div className="flex gap-4 items-center">
-          <button
-            onClick={handleClick}
-            className="bg-emerald-700 hover:bg-emerald-600 hover:cursor-pointer text-emerald-50 font-bold px-2 py-1 rounded duration-100 ease-in"
-            >
-            Click me
-          </button>
-          <p className="text-gray-700">{data.error || data.body}</p>
-        </div>
-      </div>
+        <Hero />
+        <div className="flex-1 flex flex-col items-center justify-center gap-4">
+          <FlightForm />
+          {/* <h1 className="text-3xl font-bold underline text-emerald-700">App</h1>
+            <div className="flex gap-4 items-center">
+              <button
+                onClick={handleClick}
+                className="bg-emerald-700 hover:bg-emerald-600 hover:cursor-pointer text-emerald-50 font-bold px-2 py-1 rounded duration-100 ease-in"
+                >
+                Click me
+              </button>
+              <p className="text-gray-700">{data.error || data.body}</p>
+            </div> */} 
+        </div> 
       </div>
     </div>
   )
