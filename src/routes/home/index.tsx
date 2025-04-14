ba66d221f1195740e7b7e3c48c019e79698ca751
reddish <cjcorc10@gmail.com>
2025-04-13 19:55:51 -0500

    moved container behind nav for no scroll

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -7,10 +7,10 @@ const Home = () => {
   const [data, setData] = useState<MockResponse>({ body: 'Initial Data' });
 
   return (
-    <div className="bg-green-50 font-montserrat flex items-center h-screen flex-col">
+    <div className="bg-green-50 font-montserrat flex items-center h-screen w-full flex-col absolute top-0 left-0">
       <div className="flex flex-1 flex-col md:flex-row w-full h-full">
         <Hero />
-        <div className="flex flex-2 flex-col items-center md:pt-20 overflow-auto border-l border-green-600">
+        <div className="flex flex-2 flex-col items-center justify-center md:pt-20 overflow-auto border-l border-green-600">
           <FlightForm setData={setData} />
           <p className="text-red-400 font-bold mt-4">
             {data.error || data.body}
