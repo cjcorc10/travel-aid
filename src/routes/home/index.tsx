5b88cbb2571947d29bb83f2fb4b76ed546c1d396
reddish <cjcorc10@gmail.com>
2025-04-13 19:23:20 -0500

    fixed sizing issues

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import { MockResponse } from '../../mocks/handlers'
-import FlightForm from '../../components/flightform'
-import Hero from '../../components/hero'
+import { useState } from 'react';
+import { MockResponse } from '../../mocks/handlers';
+import FlightForm from '../../components/flightform';
+import Hero from '../../components/hero';
 
 const Home = () => {
-  const [data, setData] = useState<MockResponse>({ body: 'Initial Data' })
+  const [data, setData] = useState<MockResponse>({ body: 'Initial Data' });
 
   return (
     <div className="bg-green-50 font-montserrat flex items-center h-screen flex-col">
       <div className="flex flex-1 flex-col md:flex-row w-full h-full">
         <Hero />
-        <div className="flex flex-2 flex-col items-center md:pt-20 overflow-auto sm:px-4 border-l border-green-600">
+        <div className="flex flex-2 flex-col items-center md:pt-20 overflow-auto border-l border-green-600">
           <FlightForm setData={setData} />
           <p className="text-red-400 font-bold mt-4">
             {data.error || data.body}
@@ -18,7 +18,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
