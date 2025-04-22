36e099a2cce98d7f09ec2a6be6487a44959ed261
reddish <cjcorc10@gmail.com>
2025-04-22 15:20:06 -0500

    added airport data to project

--- src/components/bookingForm/index.tsx
+++ src/components/bookingForm/index.tsx
@@ -20,31 +20,32 @@ const BookingForm = () => {
         <div className="flex flex-col">
           <label className="text-sm">From</label>
           <input
-            {...register('from')}
-            className="border rounded-md p-1 w-full shadow-md border-gray-100 bg-gray-50 text-gray-700 outline-pink-200"
+            {...register('departing')}
+            className="border rounded-md p-1 w-full shadow-md border-gray-100 text-gray-700 outline-pink-200"
             type="text"
           />
         </div>
         <div className="flex flex-col">
           <label className="text-sm">To</label>
           <input
-            {...register('to')}
-            className="border rounded-md p-1 w-full shadow-md border-gray-100 bg-gray-50 text-gray-700 outline-pink-200"
+            {...register('destination')}
+            className="border rounded-md p-1 w-full shadow-md border-gray-100 text-gray-700 outline-pink-200"
             type="text"
           />
         </div>
       </div>
       <div className="flex flex-col">
-        <label className="text-sm">Dates</label>
+        <label className="text-sm">Departure date</label>
         <input
-          className="border rounded-md p-1 w-full shadow-md border-gray-100 bg-gray-50 text-gray-700 outline-pink-200"
+          {...register('from')}
+          className="border rounded-md p-1 w-full shadow-md border-gray-100 text-gray-700 outline-pink-200"
           type="date"
         />
       </div>
       <div className="flex justify-center">
         <input
           type="submit"
-          className="bg-emerald-600 rounded-md text-white mt-2 p-1 px-2"
+          className="bg-emerald-600 shadow-md rounded-md text-white mt-2 p-1 px-2 font-bold"
           value="update search"
         />
       </div>
