bd241b9bb6cb9ee0b18ce4fcd3edd170aaa9be16
cj <cjcorc10@gmail.com>
2025-05-02 14:55:30 -0500

    fixed Popever to show selection checked

--- src/components/autoCompleteInput/index.tsx
+++ src/components/autoCompleteInput/index.tsx
@@ -26,13 +26,21 @@ const AutoCompleteInput = ({ value, onChange }: Props) => {
   const [results, setResults] = useState(airports.slice(0,0));
   const [open, setOpen] = useState(false)
 
+
+console.log(value)
   // search for airport each time letter is typed in input
   useEffect(() => {
-    if (value.length > 2) {
-      const locations = airports.filter((airport) =>
-        `${airport.airport} ${airport.city} ${airport.code}`
-          .toLowerCase()
-          .includes(value.toLowerCase())
+    if (value.length > 0) {
+      
+      const myVal = value.toLowerCase().trim().split(', ')
+
+      // filter out matching airports
+      const locations = airports.filter((airport) => {
+        const airports = `${airport.airport} ${airport.city} ${airport.code} ${airport.country}`.toLowerCase()
+
+      // search airports with each term
+      if (myVal.every(term => airports.includes(term))) return airport
+      }
       );
       // only return 10 entries max
       setResults(locations.slice(0, 9))
@@ -40,7 +48,6 @@ const AutoCompleteInput = ({ value, onChange }: Props) => {
       setResults([]);
     }
   }, [value]);
-  console.log(results);
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
@@ -48,18 +55,19 @@ const AutoCompleteInput = ({ value, onChange }: Props) => {
           variant='outline'
           role="combobox"
           aria-expanded={open}
-          className='w-full justify-between'
+          className='min-w-[200px] justify-between'
           >
-            {value 
-              ? airports.find((airport) => airport.code === value)?.code
+            {value
+              ? value.split(',', 2)
               : "Select airport..."}
             <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className='w-full p-0'>
+      <PopoverContent className='min-w-[200px] p-0'>
         <Command>
           <CommandInput 
-            
+            value={value}
+            onValueChange={(e) => onChange(e)}
             placeholder="Search airports..." />
           <CommandList>
             <CommandEmpty>No Airports found.</CommandEmpty>
@@ -67,16 +75,16 @@ const AutoCompleteInput = ({ value, onChange }: Props) => {
               {results.map((airport) => (
                 <CommandItem
                   key={airport.code}
-                  value={airport.code}
+                  value={`${airport.code}, ${airport.airport}, ${airport.city}, ${airport.country}`}
                   onSelect={(currentValue) => {
                     onChange(currentValue)
                     setOpen(false)
                   }}
                   >
                     <Check className={cn("mr-2 h-4 w-4",
-                     value === airport.code ? "opacity-100" : "opacity-0" )}
+                     value.includes(airport.code) ? "opacity-100" : "opacity-0" )}
                     />
-                    {airport.code}
+                    {`${airport.code}, ${airport.airport}, ${airport.city}, ${airport.country}`}
                   </CommandItem>
               ))}
             </CommandGroup>
@@ -84,27 +92,6 @@ const AutoCompleteInput = ({ value, onChange }: Props) => {
         </Command>
       </PopoverContent>
     </Popover>
-    // <>
-    //   <input
-    //     type="text"
-    //     value={value}
-    //     onChange={(e) => onChange(e.target.value)}
-    //     list={value}
-    //     required
-    //     placeholder="enter city or airport..."
-    //     className="border border-gray-300 rounded-lg mb-2 w-full p-1 px-2 shadow-md outline-pink-200"
-    //   />
-    //   {results && (
-    //     <datalist className="bg-white" id={value}>
-    //       {results.map((airport) => (
-    //         <option
-    //           key={airport.code}
-    //           value={`${airport.code}, ${airport.airport}, ${airport.city}, ${airport.country}`}
-    //         />
-    //       ))}
-    //     </datalist>
-    //   )}
-    // </>
   );
 };
 
